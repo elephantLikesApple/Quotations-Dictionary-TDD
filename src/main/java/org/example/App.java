34c861bede2147b08c532c2d48735c5ca9aae3aa
elephantLikesApple <elephant890@naver.com>
2023-03-06 14:52:35 +0900

    Created enhancement #1 test code

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+    public void run() {
+
+    }
+}
